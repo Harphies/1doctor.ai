3a28056c9cfcd257abd88037ec545ced12e18173
Harphies <tafeez.tech@gmail.com>
2020-06-17 10:51:38 +0100

    changes

--- src/components/sections/FeaturesTiles.js
+++ src/components/sections/FeaturesTiles.js
@@ -43,9 +43,9 @@ const FeaturesTiles = ({
   );
 
   const sectionHeader = {
-    title: "The future we Imagine",
+    title: "The future of medicine",
     paragraph:
-      "We imagine a future where people have access to quick and reliable diagnosis with personalized medical treatment through fast and reliable connectivity and Intelligent applications that know individual through their unique genomic data.",
+      "We imagine a future where people have access to quick and reliable diagnosis, personalized medical treatment based on thier unique genomic data and accurate illness prediction through fast and reliable connectivity and Intelligent applications.",
   };
 
   return (
